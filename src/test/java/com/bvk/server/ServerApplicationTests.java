c5cc7501321232b8542718924d0e9b8cd0aa402a
Arief Widiyatmoko <arief.widiyatmoko@adira.co.id>
2023-02-09 09:46:41 +0700

    init commit

---
+++ src/test/java/com/bvk/server/ServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bvk.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
